e2827e6372ff98847a1b6305991bb562923ab9c8
projetoduda <79994189+projetoduda@users.noreply.github.com>
2021-10-25 16:22:54 -0300

    Add files via upload

---
+++ 4bimestrePA/Aula1variaveis/variaveis.js
@@ -0,0 +1,5 @@
+var num = 10;
+var num2 = 200;
+var texto = "campanha Arrecadaçao";
+var cliente = "Maria Eduarda";
+pontos = num + num2;
---
+++ 4bimestrePA/Aula2entrada/entrada.js
@@ -0,0 +1,6 @@
+function pedirvalores(){
+  num1 = (prompt("Digite o primeiro numero"));
+  num2 = (prompt("Digite o segundo numero"));
+  soma = num1+num2
+  subtraçao = num1-num2
+}
